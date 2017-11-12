937d68fee8f6a054e2eaf7bc7e2e871cd710bf07
Hubert Nguyen <hubertanguyen@gmail.com>
2017-11-12 14:02:56 -0600

    add build task for html and styles

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,6 @@
 // Require modules;
 const gulp = require ("gulp");
-const gulpSass = require("gulp-sass");
+const sass = require("gulp-sass");
 const connect = require("gulp-connect");
 const sourceMaps = require("gulp-sourcemaps");
 const bable = require("gulp-babel");
@@ -12,17 +12,46 @@ const bable = require("gulp-babel");
     4.) babel
 */
 
-gulp.task("default", function() {
-    console.log("default task");
-});
+gulp.task("default", ["html","watch","serve"]);
 
+// Starts the connect server
 gulp.task("serve", function() {
     console.log("start server");
+    console.log("run server task");
     connect.server({
-        root: "src",
+        root: "build",
         livereload: true
     });
 });
+
+// reloads the server pass in the html file
+gulp.task("html", function() {
+    console.log("run html task");
+    gulp.src("./src/**.html")
+        .pipe(gulp.dest("build"))
+        .pipe(connect.reload()); 
+});
+
+// Watches for changes in html,css,js
+gulp.task("watch", function() {
+    console.log("start watching files");
+    gulp.watch("./src/**.html",["html"]);
+    gulp.watch("./src/scss/**/*.scss",["sass"]);
+});
+
+// Convert sass to css
+gulp.task("sass", function() {
+    return gulp.src("./src/scss/**/*.scss") 
+        .pipe(sass().on("error",sass.logError))
+        .pipe(gulp.dest("./build/css"))
+        .pipe(connect.reload());
+});
+
+
+
+
+
+
 
 
 
