00031992c40c78273fe3f3fef0ec2e5dda6eeb1d
hubertanguyen@gmail.com <hubertnguyen@Huberts-MacBook-Pro.local>
2017-12-29 22:47:13 -0600

    feat: add test for simple item

---
+++ jasmine-standalone-2.8.0/spec/SimpleItemSpec.js
@@ -0,0 +1,28 @@
+describe("Simple Item", function() {
+    var item;   
+
+    beforeEach(function() {
+        item = new SimpleItem();
+    });
+
+    it("should be a function constructor", function() {
+
+    });
+
+    it("should give an error if it's created without a description ", function() {
+
+    });
+
+    it("should have a description, which is a string", function() {
+
+    });
+
+    it("should have a due date, whihc is date object", function(){
+
+    });
+
+    it("should have a complete status, which is a boolean ", function(){
+
+    });
+
+});
