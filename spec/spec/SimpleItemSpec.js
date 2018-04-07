9a4ccf7d6ce5423253f5234725b628912550af1c
hubertanguyen@gmail.com <hubertnguyen@Huberts-MacBook-Pro.local>
2018-04-06 23:13:40 -0500

    add specs for simple item class and instance

--- spec/spec/SimpleItemSpec.js
+++ spec/spec/SimpleItemSpec.js
@@ -8,51 +8,87 @@ describe("Simple Item Class", function() {
     });
 
     it("should be a function constructor", function() {
-        console.log(SimpleItem);
-        expect(typeof SimpleItem).toBe("function");
+      expect(typeof SimpleItem).toBe("function");
     });
 
-    if("should have a class name of 'Simple Item'", function() {
-
+    it("should have a class name of 'SimpleItem'", function() {
+      expect(SimpleItem.constructor.name.toBe("SimpleItem");
     });
-
-    it("should be able to set a date", function() {
-        expect(item.duedate).toBe("N/A");
-        expect(typeof item.setDate).toBe("function");
-        item.setDate(new Date);
-        expect(item.dueDate.constructor.name).toBe("Date");  
-    }); 
-    
-    it("should have a complete status, which is a boolean; it defaults to false", function(){
-        expect(typeof item.complete).toBe("boolean");  
-        expect(item.compelte).toBe(false);
-    });
-
 });
 
 describe("Simple Item Instance", function() {
+    var item;   
+
+    beforeEach(function() {
+        item = new SimpleItem({
+            desc: "Do Laundry"
+        });
+    });
+
 
     it("should return an error if it's created without a description property", function() {
-        expect(new SimpleItem().constructor.name).toBe("Error");
+      expect(new SimpleItem().constructor.name).toBe("Error");
     });
 
-    it("should be able to retreive it's description property", function() {
-        expect(item.desc).toBe("Do Laundry");
+    it("should have a getter function named 'getDesc'", function() {
+      expect(item.getDesc)toBeDefined();  
+      expect(typeof item.getDesc).toBe("function"); 
+    });
+
+    it("'getDesc' method should return it's description value", function() {
+      expect(item.getDesc()).toBe("Do Laundry");
+      let brandNewItem = new SimpleItem({desc: "Take out the trash."});
+      expect(brandNewItem.getDesc()).toBe("Take out the trash.");
+    });
+   
+    it("should have a setter function named 'setDesc'", function() {
+      expect(item.setDesc).toBeDefined();
+      expect(typeof item.setDesc).toBe("function");
+    });
+
+    it("'setDesc' method should set it's description", function() {
+        expect(item.getdesc()).toBe("Do Laundry");
         item.setDesc("Wash Dishes");
         expect(item.desc).toBe("Wash Dishes");
     });
 
-    it("should be able to set a description", function() {
-        expect(item.desc).toBe("Do Laundry");
-        item.setDesc("Wash Dishes");
-        expect(item.desc).toBe("Wash Dishes");
+    it("should have a getter function named 'getStatus')", function() {
+      expect(item.getStatus).toBeDefined();
+      expect(typeof item.getStatus).toBe("function");
+    });
+
+    it("'getStatus' method should return the status", function() {
+      let lawnItem = new SimpleItem({
+        desc: "Mow the Lawn",
+        complete: true
+      });
+
+      let cookDinnerItem = new SimpleItem({
+        desc: "Cook Dinner",
+        complete: false
+      });
+      expect(lawnItem.getStatus()).toBe(true);
+      expect(cookDinnerItem.getStatus().toBe(false);
+    });
+
+    it("should default with a complete property which equals to false", function(){
+        expect(typeof item.getStatus()).toBe("boolean");  
+        expect(item.getStatus()).toBe(false);
+    });
+
+    it("should have a setter method named, 'setStatus'", function() {
+
+    });
+
+    it("'setStatus' method should set the complete property, it must accept a boolean", function() {
+
     });
 
     xit("should have a default due date property, which is 'N/A'", function(){
         expect(item.dueDate).toBe("N/A");
     });
 
-    xit("should have a setter function for it's due date property", function() {
+    xit("should have a setter function to set it's due date property", function() {
         expect(item.setDate).toBeDefined();
         expect(typeof item.setDate).toBe("function");
     });
